f04e4ae32da8e1ca87a464e7626078902ef8377c
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2024-03-07 15:05:56 +0000

    count-bits

--- guia-pratico/count_bits.c
+++ guia-pratico/count_bits.c
@@ -1,3 +1,11 @@
-void count_bits( int val ) {
+#include <stdio.h>
 
+void count_bits( int val )
+{
+	// int in C is 32 bits we start on the most relevant bit
+	for(int i = 31; i >= 0; i--)
+	{
+        printf("%d", (val >> i) & 1);
+    }
+    printf("\n");
 }
---
+++ guia-pratico/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void count_bits( int val );
+
+int main()
+{
+	count_bits(0x3);
+	return 0;
+}
