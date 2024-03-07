53081e1799cc1e3204fb43ee353f3b206622feb0
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2024-03-07 16:09:32 +0000

    print_fibonnaci

--- guia-pratico/count_bits.c
+++ guia-pratico/count_bits.c
@@ -2,10 +2,16 @@
 
 void count_bits( int val )
 {
+	int count0 = 0;
+	int count1 = 0;
 	// int in C is 32 bits we start on the most relevant bit
 	for(int i = 31; i >= 0; i--)
 	{
-        printf("%d", (val >> i) & 1);
+		if(((val >> i) & 1) == 0) {
+			count0++;
+		} else {
+			count1++;
+		}
     }
-    printf("\n");
+    printf("bits 0: %d\nbits 1: %d\n", count0, count1);
 }
--- guia-pratico/test.c
+++ guia-pratico/test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 void count_bits( int val );
+void print_fibonnaci( int n );
 
 int main()
 {
-	count_bits(0x3);
+	int n = 42;
+	printf("\n ------------------ \n");
+	printf("\ncount_bits(%d):\n", n);
+	count_bits(n);
+	printf("\n ------------------ \n");
+	printf("\nprint_fibonnaci(%d):\n", n);
+	print_fibonnaci(n);
+	printf("\n ------------------ \n");
 	return 0;
 }
