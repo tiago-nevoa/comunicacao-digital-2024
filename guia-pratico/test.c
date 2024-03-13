02914206177a84c25bdf1c085dd17d663ec83c3a
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2024-03-13 18:13:40 +0000

    reverse_file : with bug when writing in outpit file -> find and solve

--- guia-pratico/test.c
+++ guia-pratico/test.c
@@ -4,11 +4,13 @@ void count_bits(int val);
 void print_fibonacci(int n);
 int file_symbol_freq(char *file_name, char symbol);
 void file_histogram(char *file_name);
+void reverse_file(char *input_file_name, char *output_file_name);
 
 int main()
 {
 	int n = 42;
-	char my_first_file[] = "test.txt";
+	char my_input_file[] = "test-in.txt";
+	char my_output_file[] = "test-out.txt";
 	char symbol = 'e';
 
 	printf("\n ------------------ \n");
@@ -20,12 +22,17 @@ int main()
 	print_fibonacci(n);
 
 	printf("\n ------------------ \n");
-	printf("\nfile_symbol_freq('%c') : %d\n", symbol, file_symbol_freq(my_first_file, symbol));
+	printf("\nfile_symbol_freq('%c') : %d\n", symbol, file_symbol_freq(my_input_file, symbol));
 	printf("\n ------------------ \n");
 
 	printf("\n ------------------ \n");
-	printf("\nfile_histogram('%s') :\n", my_first_file);
-	file_histogram(my_first_file);
+	printf("\nfile_histogram('%s') :\n", my_input_file);
+	file_histogram(my_input_file);
+	printf("\n ------------------ \n");
+
+	printf("\n ------------------ \n");
+	printf("\nreverse_file('%s', '%s') :\n", my_input_file, my_output_file);
+	reverse_file(my_input_file, my_output_file);
 	printf("\n ------------------ \n");
 
 	return 0;
